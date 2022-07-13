061e73cb51f5c6d4a0fa03e35f5b0d753fd14ea4
Samara <maiareismaia@gmail.com>
2022-07-12 21:21:21 -0300

    aula 36 Requisição simples

---
+++ src/Config.js
@@ -0,0 +1,5 @@
+const Config = {
+  BASE_URL: '/projeto2/public'
+}
+
+export default Config
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,34 @@
+import { tree } from 'gulp'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ip: ''
+    }
+  }
+  componentDidMount() {
+    fetch('http://ip.jsontest.com/')
+      .then(r => {
+        return r.json()
+      })
+      .then(json => {
+        this.setState({ ip: json.ip })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Página Home...</h3>
+        <div>
+          {this.state.ip == '' && <i>Carregando...</i>}
+          {this.state.ip != '' && <p>Seu IP é: {this.state.ip}</p>}
+        </div>
+        <Link to="/sobre">Ir para página sobre</Link>
+      </div>
+    )
+  }
+}
